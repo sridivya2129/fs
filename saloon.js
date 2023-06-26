af3ef6cc4a71b2f2150b394c167727d229a41870
sridivya <129477222+sridivya2129@users.noreply.github.com>
2023-06-26 20:03:56 +0530

    Add files via upload

---
+++ saloon.js
@@ -0,0 +1,114 @@
+$('.car-2').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    dots:false,
+    nav:false,
+    
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        900:{
+            items:4
+        }
+    }
+    })
+    
+    
+     $('.car-1').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+    
+    
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        900:{
+            items:6
+        }
+    }
+    })
+
+    $('.car-3').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+    
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        900:{
+            items:3
+        }
+    }
+    })
+
+    $('.home-car').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:false,
+       
+        
+        
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            900:{
+                items:1
+            }
+        }
+        })
+
+        $('.brands').owlCarousel({
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:3000,
+            dots:false,
+            nav:false,
+           
+            
+            
+            responsive:{
+                0:{
+                    items:2
+                },
+                600:{
+                    items:3
+                },
+                900:{
+                    items:6
+                }
+            }
+            })
+
+            
